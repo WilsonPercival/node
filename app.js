9f892aa512ecaa92f8adc9c96586c833b192b4a0
Wilson <91274932+WilsonPercival@users.noreply.github.com>
2022-06-20 22:38:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ const logFileName = `logs/log_${new Date().toString().split(`:`).join(``)}.txt`;
 write_log(new Date().toString());
 
 server({port: 8089});
-const arg2 = process.argv[2] === undefined ? "jes.princess" : process.argv[2];
+const arg2 = process.argv[2] === undefined ? "simbochka" : process.argv[2];
 tiktok({tiktokUsername: arg2});
 
 
